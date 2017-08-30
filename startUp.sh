b04adc54d1a0c37e4bd5cec885c8f867a56d336e
David Buschman <david.buschman@timeli.io>
2017-08-30 17:59:45 -0600

    initial notebooks created

--- startUp.sh
+++ startUp.sh
@@ -10,7 +10,9 @@ mkdir -p notebooks
 
 # Run the jupyter container 
 docker run -it --rm \
-	-p 8888:8888 \
-	-v `pwd`/notebooks:/home/jovyan/work \
 	--name jupyter-spark \
+	-p 8888:8888 \
+	-e NB_UID=$(id -u) \
+	-e NB_GID=$(id -g) \
+	-v `pwd`/notebooks:/home/jovyan \
 	jupyter/all-spark-notebook
